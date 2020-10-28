2979dc7d4b6a069a342e4955c1947d8ee9f7ae48
Luigi Angotti <lopolapo93@gmail.com>
2020-10-28 13:42:48 +0100

    primo commit

---
+++ testPrimo/src/main/java/ingsw/testPrimo/App.java
@@ -0,0 +1,13 @@
+package ingsw.testPrimo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
